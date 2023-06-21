104192d31c5aceede434a6b5166108e58e6e4b3d
kiralydaniel <kiralydani93@gmail.com>
2023-06-21 19:23:06 +0200

    Add BasePage.cs

---
+++ main/pageFactory/BasePage.cs
@@ -0,0 +1,37 @@
+﻿using Jira.main.pageFactory;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Utilities;
+using OpenQA.Selenium;
+using System;
+
+namespace Jira.main.pageFactory
+{
+    public class BasePage
+    {
+        protected IWebDriver driver;
+        string browserType = Environment.GetEnvironmentVariable("browserType");
+
+        public BasePage()
+        {
+            if (browserType == null)
+            {
+                browserType = "Chrome";
+            }
+            this.driver = WebDriverFactory.CreateWebDriver(browserType);
+        }
+
+        public void NavigateTo(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public void RefreshPage()
+        {
+            driver.Navigate().Refresh();
+        }
+
+        public static void Teardown()
+        {
+            WebDriverFactory.ShutdownWebDriver();
+        }
+    }
+}
