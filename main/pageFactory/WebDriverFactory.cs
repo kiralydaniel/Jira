fae52971b4799785aa31decfbf00e6fb15357911
kiralydaniel <kiralydani93@gmail.com>
2023-06-21 15:35:33 +0200

    Add WebDriverFactory.cs

---
+++ main/pageFactory/WebDriverFactory.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace Jira.main.pageFactory
+{
+    public class WebDriverFactory
+    {
+        private static IWebDriver driver;
+
+        public static IWebDriver CreateWebDriver(string browserType)
+        {
+            switch (browserType)
+            {
+                case "Chrome":
+                    driver = CreateChromeDriver();
+                    break;
+                case "Firefox":
+                    driver = CreateFirefoxDriver();
+                    break;
+            }
+
+            driver.Manage().Window.Maximize();
+
+            return driver;
+        }
+
+        private static IWebDriver CreateChromeDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            return new ChromeDriver(options);
+        }
+
+        private static IWebDriver CreateFirefoxDriver()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            return new FirefoxDriver(options);
+        }
+
+        public static void ShutdownWebDriver()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+                driver = null;
+            }
+        }
+    }
+
+}
