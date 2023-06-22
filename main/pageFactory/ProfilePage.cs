1667e1dea68d3d485013a31e79d1f774d07cca02
kiralydaniel <kiralydani93@gmail.com>
2023-06-22 18:28:54 +0200

    Add ProfilePage.cs

---
+++ main/pageFactory/ProfilePage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jira.main.pageFactory
+{
+    public class ProfilePage : BasePage
+    {
+        private readonly By usernameLocator = By.Id("up-d-username");
+
+        private IWebElement username;
+
+        public ProfilePage() : base() 
+        { 
+        }
+
+        public string GetUsername()
+        {
+            wait.Until(ExpectedConditions.ElementIsVisible(usernameLocator));
+            username = driver.FindElement(usernameLocator);
+            return username.Text;
+        }
+
+    }
+}
