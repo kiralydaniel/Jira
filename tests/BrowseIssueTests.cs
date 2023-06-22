7799aa948efbdedf5ebdf5d7cf4dd5afb08e8bac
kiralydaniel <kiralydani93@gmail.com>
2023-06-23 01:22:01 +0200

    Add BrowseIssueTests

---
+++ tests/BrowseIssueTests.cs
@@ -0,0 +1,49 @@
+﻿using Jira.main.pageFactory;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jira.tests
+{
+    [TestFixture]
+    public class BrowseIssueTests
+    {
+        private LoginPage loginPage;
+        private IssuePage issuePage;
+        private DashboardPage dashboardPage;
+
+        [SetUp]
+        public void Init()
+        {
+            loginPage = new LoginPage();
+            issuePage = new IssuePage();
+            dashboardPage = new DashboardPage();
+            loginPage.NavigateTo(Util.baseURL);
+            loginPage.LoggingIn(Util.username, Util.password);
+        }
+
+        public void BrowseIssue(string issueUrl, string expectedIssueKey)
+        {
+            dashboardPage.WaitForProfileBtn();
+            loginPage.NavigateTo(issueUrl);
+            Assert.That(expectedIssueKey, Is.EqualTo(issuePage.GetIssueKey()));
+        }
+
+        [Test]
+        [TestCaseSource(typeof(Util), nameof(Util.BrowseIssueTestData), new object[] { "main/resources/issues.csv" })]
+        public void BrowseIssueTest(string issueUrl, string expectedIssueKey)
+        {
+            BrowseIssue(issueUrl, expectedIssueKey);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            BasePage.Teardown();
+        }
+    }
+}
