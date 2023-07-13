e671ea14371bbd3d5095edfd5f6b3ed3ea8a09dc
kiralydaniel <kiralydani93@gmail.com>
2023-07-13 02:38:55 +0200

    Add CreateIssueTests.cs

---
+++ Tests/CreateIssueTests.cs
@@ -0,0 +1,77 @@
+﻿using Jira.Main.PageFactory;
+using NUnit.Framework;
+using System.Collections;
+
+namespace Jira.Tests
+{
+    [TestFixture]
+    public class CreateissueTests
+    {
+        private LoginPage loginPage;
+        private IssuePage issuePage;
+        private DashboardPage dashboardPage;
+        private static string createTestString = "New Issue1";
+        private static string summaryErrorMsg = "You must specify a summary of the issue.";
+
+        [SetUp]
+        public void Init()
+        {
+            loginPage = new LoginPage();
+            issuePage = new IssuePage();
+            dashboardPage = new DashboardPage();
+            loginPage.NavigateTo(Util.BaseURL);
+            loginPage.LoggingIn(Util.Username, Util.Password);
+        }
+
+        private void CancelCreation(string project, string type, string summary)
+        {
+            dashboardPage.ClickCreateBtn();
+            dashboardPage.FillProjectField(project);
+            dashboardPage.FillTypeField(type);
+            dashboardPage.FillSummaryField(summary);
+            dashboardPage.ClickCancelIssueBtn();
+        }
+
+        private void SuccessfulCreateIssue(string project, string type, string summary)
+        {
+            dashboardPage.ClickCreateBtn();
+            dashboardPage.FillProjectField(project);
+            dashboardPage.FillTypeField(type);
+            dashboardPage.FillSummaryField(summary);
+            dashboardPage.ClickCreateIssueBtn();
+            dashboardPage.ClickCreatedIssueLink();
+            Assert.That(type, Is.EqualTo(issuePage.GetTheType()));
+            Assert.That(summary, Is.EqualTo(issuePage.GetSummary()));
+            issuePage.DeleteIssue();
+            Assert.IsTrue(issuePage.DeletedIssueValidate());
+        }
+
+        [Test]
+        public void EmptyFields()
+        {
+            dashboardPage.ClickCreateBtn();
+            dashboardPage.ClickCreateIssueBtn();
+            Assert.That(summaryErrorMsg, Is.EqualTo(dashboardPage.GetSummaryErrorMsg()));
+        }
+
+        [Test]
+        [TestCaseSource(typeof(Util), nameof(Util.TestData), new object[] { "Main/Resources/createIssue.csv" })]
+        public void CreateIssue(string project, string type)
+        {
+            SuccessfulCreateIssue(project, type, createTestString);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(Util), nameof(Util.TestData), new object[] { "Main/Resources/cancelIssue.csv" })]
+        public void CancelIssueCreation(string project, string type)
+        {
+            CancelCreation(project, type, createTestString);
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            BasePage.Shutdown();
+        }
+    }
+}
